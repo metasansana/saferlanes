a3e2a9e44a6e7416b5fe4ec0b43e28a445c3aa55
developer <lmurray@trinistorm.org>
2012-06-24 11:07:30 -0400

    Moved to the net package. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/callow/net/MalFormedUrlException.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * timestamp Jun 24, 2012 1:59:22 AM
+ *
+ *
+ * @author Lasana Murray  <lmurray@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package calow\net
+ *
+ *
+ */
+
+namespace callow\net;
+
+class MalFormedUrlException extends \Exception
+{
+
+    /**
+     *
+     * @var
+     * @access protected
+     */
+
+}
+
+?>
