147d232edc297a61108317c8542304251d204765
developer <lmurray@trinistorm.org>
2012-06-29 10:49:17 -0400

    Moved to saferlanes\core package. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/saferlanes/core/Driver.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * timestamp May 30, 2012 12:00:39 AM
+ *
+ *
+ * @author Lasana Murray  <lmurray@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package saferlanes\domain;
+ *
+ *
+ */
+
+namespace saferlanes\core;
+
+use callow\domain\AbstractDomain;
+use callow\domain\BadValueException;
+use callow\dbase\DatabaseMapper;
+
+class Driver extends AbstractDomain implements DriverInterface
+{
+
+    protected $plate;
+    protected $timestamp;
+    protected $plus;
+    protected $minus;
+
+    public function __construct(array $driver = NULL)
+    {
+        if ($driver)
+            $this->set($driver);
+
+    }
+
+    public function setPlate($plate)
+    {
+
+        $validator = new DriverValidator();
+
+        $plate = strtolower(str_replace(' ', NULL, trim($plate)));
+
+        if (!$validator->isValid('plate', $plate))
+        {
+            throw new BadValueException('plate', $plate, $validator->getError('plate'));
+        }
+        else
+        {
+            $this->plate = $plate;
+        }
+
+
+        return $this;
+
+    }
+
+    public function setTimeStamp()
+    {
+
+        $this->timestamp = time();
+        return $this;
+
+    }
+
+    public function setPlus($plus)
+    {
+
+        $validator = new DriverValidator();
+
+        if (!$validator->isValid('plus', $plus))
+        {
+            throw new BadValueException('plus', $plus, $validator->getError('plus'));
+        }
+
+        return $this;
+
+    }
+
+    public function setMinus($minus)
+    {
+
+        $validator = new DriverValidator();
+
+        if (!$validator->isValid('minus', $minus))
+        {
+            throw new BadValueException('minus', $minus, $validator->getError('minus'));
+        }
+
+        return $this;
+
+    }
+
+    public function getPlate()
+    {
+        return $this->plate;
+
+    }
+
+    public function getTimeStamp()
+    {
+        return $this->timestamp;
+
+    }
+
+    public function getPlus()
+    {
+        return $this->plus;
+
+    }
+
+    public function getMinus()
+    {
+        return $this->minus;
+
+    }
+
+
+}
+
+?>
---
+++ includes/saferlanes/core/DriverValidator.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * timestamp May 30, 2012 1:30:50 PM
+ *
+ *
+ * @author Lasana Murray  <lmurray@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package saferlane\core\domain
+ *
+ *
+ */
+
+namespace saferlanes\core;
+
+use callow\domain\Validator;
+
+class DriverValidator implements Validator
+{
+
+    private $errors = array ();
+
+    private function _validatePlate($plate)
+    {
+        $pattern = '/^(p|d|r|h|v|x|t{1})([a-z]){0,2}(\d){1,4}$/';
+
+        if (!preg_match($pattern, $plate))
+        {
+            $this->errors['plate'] = "Hey! That's not a valid plate number.";
+            return FALSE;
+        }
+        else
+        {
+            return TRUE;
+        }
+
+    }
+
+    /**
+     * Validates the plus attribute.
+     * @param string $plus
+     * @return boolean
+     */
+    private function _validatePlus($plus)
+    {
+        if ($plus !== 'plus')
+        {
+            $this->errors['plus'] = "Whoops! We have a tainted vote here! Try again.";
+            return FALSE;
+        }
+
+        return TRUE;
+
+    }
+
+    /**
+     * Validates the minus attribute.
+     * @param string $minus
+     * @return boolean
+     */
+    private function _validateMinus($minus)
+    {
+        if ($minus !== 'minus')
+        {
+            $this->errors['minus'] = "Whoops! We have a tainted vote here! Try again.";
+            return FALSE;
+        }
+
+        return TRUE;
+
+    }
+
+    public function isValid($property, $value)
+    {
+
+        $result = NULL;
+
+        switch ($property)
+        {
+
+            case 'plate': $result = $this->_validatePlate($value);
+                break;
+
+            case 'plus': $result = $this->_validatePlus($value);
+                break;
+
+            case 'minus': $result = $this->_validateMinus($value);
+                break;
+
+            default: $result = FALSE;
+                break;
+        }
+        return $result;
+
+    }
+
+    public function getError($name)
+    {
+        if (array_key_exists($name, $this->errors))
+            return $this->errors[$name];
+
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+
+    }
+
+}
+
+?>
