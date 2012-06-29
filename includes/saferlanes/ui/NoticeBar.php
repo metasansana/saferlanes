fbfb2ab326735f7fb20d6765153de9388d3aa467
developer <lmurray@trinistorm.org>
2012-06-28 21:53:02 -0400

    NoticeBar added to the saferlanes\ui package. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/saferlanes/ui/NoticeBar.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * timestamp Jun 9, 2012 12:50:26 PM
+ *
+ *
+ * @author Lasana Murray  <lmurray@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package callow\ui
+ *
+ *  Class for sending messages to the screen after an operation.
+ *
+ */
+
+namespace saferlanes\gui;
+
+use callow\ui\Widget;
+
+class NoticeBar extends Widget
+{
+
+public function setNotice($msg)
+{
+    if(is_string($msg))
+    {
+
+        $msg = "<div class='notice'>$msg</div>";
+        return $this;
+    }
+
+}
+
+}
+
+?>
