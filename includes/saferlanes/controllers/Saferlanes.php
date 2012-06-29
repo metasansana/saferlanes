d9f12ad45bbc3cf8864daf1140d0a668443cc791
developer <lmurray@trinistorm.org>
2012-06-29 07:53:40 -0400

    New class. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/saferlanes/controllers/Saferlanes.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * timestamp Jun 29, 2012 7:52:39 AM
+ *
+ *
+ * @project saferlanes
+ * @author Lasana Murray  <lmurray@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package saferlanes\controllers
+ *
+ *  The main class of the saferlanes application.
+ *
+ */
+
+namespace callow\controllers;
+
+class Saferlanes
+{
+
+    /**
+     *
+     * @var
+     * @access protected
+     */
+
+}
+
+?>
