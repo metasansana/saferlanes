3087200348bbbff2ad976ab6932c27be6518a64b
developer <lmurray@trinistorm.org>
2012-06-28 23:25:50 -0400

    Added ADMIN_EMAIL and SL_TEMPLATE_PATH settings. Signed-off-by:developer <lmurray@trinistorm.org>

--- includes/saferlanes/scripts/settings.php
+++ includes/saferlanes/scripts/settings.php
@@ -4,9 +4,15 @@
 // @author Lasana Murray  <lmurray@trinistorm.org>
 // @project saferlanes
 // Copyright (C) 2012 Lasana Murray
+//XXX Some of these constatns must be changed per server install!
 
 define('VERSION', '0.5a');
 
 define('NAME', 'Saferlanes');
 
+define('ADMIN_EMAIL', 'lmurray@trinistorm.org');
+
+define('SL_TEMPLATE_PATH', '/srv/www/saferlanes/includes/saferlanes/templates');
+
 ?>
+
