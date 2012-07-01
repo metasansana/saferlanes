d072b5876d44f980ead9823cdfc28ea08314de5b
developer <lmurray@trinistorm.org>
2012-07-01 01:14:26 -0400

    Created the Error class. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/callow/galaxy/Error.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * timestamp Jul 1, 2012 1:13:25 AM
+ *
+ *
+ * @author Lasana Murray  <lmurray@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package callow\galaxy
+ *
+ *  Exception type class that is thrown when the user does something wrong.
+ *
+ */
+class Error extends \Exception
+{
+
+
+
+}
+
+?>
