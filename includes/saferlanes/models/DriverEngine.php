1751b6551d898e5fc7611e1a292a3bcf46b6a98e
developer <lmurray@trinistorm.org>
2012-07-01 09:05:16 -0400

    DriverExtractor->DriverEngine Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/saferlanes/models/DriverEngine.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * timestamp Jun 29, 2012 10:11:05 AM
+ *
+ *
+ * @author Lasana Murray  <lmurray@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package saferlanes\models;
+ *
+ *  Helper class for manipulating driver data.
+ *
+ */
+
+namespace saferlanes\models;
+
+use callow\dbase\SQL;
+use callow\dbase\ActiveDatabase;
+use callow\dbase\DatabaseMapper;
+use saferlanes\core\DriverInterface;
+
+class DriverEngine
+{
+
+    private $dbase;
+
+    public function __construct(ActiveDatabase &$dbase)
+    {
+        $this->dbase = $dbase;
+    }
+
+    public function find(DriverInterface &$driver, SQL &$sql)
+    {
+
+
+        $mapper = new DatabaseMapper($driver, $sql, $dbase);
+
+        $mapper->load();
+
+        return $driver;
+
+    }
+}
+
+
+?>
