0cfa196b516b94c916289770cb6419d4a3b3d150
developer <lmurray@trinistorm.org>
2012-07-06 22:39:02 -0400

    Cleaner settings file. Signed-off-by:developer <lmurray@trinistorm.org>

--- includes/saferlanes/scripts/settings.php
+++ includes/saferlanes/scripts/settings.php
@@ -1,24 +1,15 @@
 <?php
-
-// @date Jun 11, 2012 6:36:29 PM
-// @author Lasana Murray  <lmurray@trinistorm.org>
-// @project saferlanes
-// Copyright (C) 2012 Lasana Murray
-//XXX Some of these constatns must be changed per server install!
-
-define('VERSION', '0.5a');
-
-define('NAME', 'Saferlanes');
-
-define('ADMIN_EMAIL', 'lmurray@trinistorm.org');
-
-define('SL_TEMPLATE_PATH', '/srv/www/saferlanes/includes/saferlanes/templates');
-
-define('CLASS_LOADER_PATH', '/srv/www/saferlanes/includes/saferlanes/scripts/classloader.php');
-
-require_once 'dbcreds.php';
-
-require_once 'classloader.php';
+/**
+ * Place settings for the application in here.
+ * Example:
+ *
+ * define('DB_USER', 'root');
+*
+*  define('DB_PASSWORD', 'password');
+*
+*  define('E_ADMIN', 'nospam@saferlanes.org');
+*
+*/
 
 ?>
 
