d071cc38a31be7a3b30c86a6ef43eeed7a78757f
developer <lmurray@trinistorm.org>
2012-07-07 23:17:24 -0400

    Renamed NullResultSetException -> EmptyResultException Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/callow/dbase/EmptyResultException.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * timestamp May 31, 2012 8:48:21 PM
+ *
+ *
+ * @project roadtroll
+ * @author Lasana Murray  <lmurray@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package callow\dbase
+ *
+ *  Thrown when a query to a database returns empty.
+ *
+ */
+
+namespace callow\dbase;
+
+class EmptyResultException extends \Exception
+{
+
+}
+
+?>
