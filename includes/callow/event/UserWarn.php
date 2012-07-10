8c68e4d0aa9f1d1ee8142c1ea131c1d5a03c6e40
developer <lmurray@trinistorm.org>
2012-07-10 17:59:54 -0400

    Added user level warnings, Warning -> Warn Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/callow/event/UserWarn.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * timestamp Jul 10, 2012 5:57:40 PM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package callow\event
+ *
+ *  The UserWarn  class enscapulates a warning  event that the user should be made aware of.
+ *
+ */
+
+namespace callow\event;
+
+class UserWarn extends Warn
+{
+
+
+
+}
+
+?>
