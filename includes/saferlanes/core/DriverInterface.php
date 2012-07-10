f5d85d165a1cdddfc440a49f4852b95cc094e456
developer <lmurray@trinistorm.org>
2012-07-10 04:26:30 -0400

    Moved getters to separate interface. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/saferlanes/core/DriverInterface.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * timestamp May 30, 2012 2:55:47 PM
+ *
+ *
+ * @author Lasana Murray  <lmurray@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package saferlanes\core\domain
+ *
+ *
+ */
+
+namespace saferlanes\core;
+
+use callow\domain\Domain;
+
+interface DriverInterface extends Domain, DriverProvider
+{
+
+    /**
+     * Sets a plate number to the Driver.
+     * @var string $plate
+     */
+    public function setPlate($plate);
+
+    /**
+     * Sets  the minus vote flag for a Driver.
+     * @var string minus
+     */
+    public function setMinus($minus);
+
+    /**
+     * Sets  the plus vote flag for a Driver.
+     *@var string $plus
+     */
+    public function setPlus($plus);
+
+    /**
+     * Sets the timestamp for a Driver;
+     */
+    public function setTimeStamp();
+
+
+}
+
+?>
---
+++ includes/saferlanes/core/DriverPresenter.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * timestamp Jul 10, 2012 4:16:30 AM
+ *
+ *
+ * @project saferlanes
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package saferlanes\core
+ *
+ *
+ */
+
+namespace saferlanes\core;
+
+class DriverPresenter implements DriverProvider
+{
+
+    /**
+     *
+     * @var
+     * @access protected
+     */
+    private $driver;
+
+    public function __construct(Driver &$driver)
+    {
+        $this->driver = $driver;
+    }
+
+
+    private function getTimeStamp()
+    {
+        $date = getdate($this->driver->getTimeStamp());
+
+        $date = "{$date['mday']} {$date['month']} {$date['year']}";
+
+        return $date;
+
+    }
+
+    private function getPlate()
+    {;
+
+        return strtoupper($this->driver->getPlate());
+
+    }
+
+   public function getPlus()
+   {
+       return $this->driver->getPlus();
+   }
+
+   public function getMinus()
+   {
+
+       return $this->driver->getMinus();
+   }
+
+
+
+    public  function getImage()
+    {
+
+        $likes =  $this->driver->getPlus();
+
+        $fails  = $this->driver->getMinus();
+
+        if($likes > $fails)
+        {
+            $image = "happy";
+        }
+        elseif ($likes == $fails)
+        {
+            $image = "unsure";
+        }
+        else
+        {
+            $image = "sad";
+        }
+
+
+        return $image;
+
+
+    }
+
+}
+
+?>
