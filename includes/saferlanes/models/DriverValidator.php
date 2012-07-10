26909a1ccd9f37b16faabdbea26aa43776fc94c1
developer <lmurray@trinistorm.org>
2012-07-10 05:08:18 -0400

    Clean up Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/saferlanes/models/DriverValidator.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * timestamp Jul 7, 2012 11:14:00 AM
+ *
+ *
+ * @author Lasana Murray  <lmurray@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package saferlanes\models;
+ *
+ * This class wraps the validation process to make it reusable.
+ *
+ *
+ */
+
+namespace saferlanes\models;
+
+use callow\event\Observer;
+use saferlanes\core\DriverObject;
+use saferlanes\core\Driver;
+use saferlanes\core\BadPlateNumberException;
+
+
+class DriverValidator
+{
+
+    /**
+     * Internal Driver implementing object
+     * @var Driver $driver
+     * @access private
+     */
+    private $driver;
+
+
+    public function __construct(Driver &$driver)
+    {
+
+        $this->driver = $driver;
+
+    }
+
+    public function assignPlateNumber($pnum)
+    {
+
+        try
+        {
+        $this->driver->setPlate($pnum);
+        }
+        catch(BadPlateNumberException $bex)
+        {
+
+            $this->fire(new Warning($bex));
+
+            return FALSE;
+        }
+
+        return TRUE;
+
+
+    }
+
+
+}
+
+
+?>
