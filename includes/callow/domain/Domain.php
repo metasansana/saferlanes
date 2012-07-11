399cfa4822b57cbeb9e4692ebf0e48832c6300d8
developer <lmurray@trinistorm.org>
2012-07-10 21:02:04 -0400

    Swapped Signed-off-by:developer <lmurray@trinistorm.org>

--- includes/callow/domain/Domain.php
+++ includes/callow/domain/Domain.php
@@ -1,30 +1,96 @@
 <?php
 
 /**
- * timestamp May 29, 2012 5:27:06 AM
+ * timestamp May 29, 2012 9:28:53 PM
  *
  *
  * @author Lasana Murray  <lmurray@trinistorm.org>
  * @copyright 2012 Lasana Murray
  * @package callow\domain
  *
- *  Interface for objects that represent data domains.
+ *  Abstract data domain object.
+ *
  */
 
 namespace callow\domain;
 
-interface Domain
+
+class Domain implements DomainInterface
 {
 
-    public function isProperty($property);
+    public function count()
+    {
+        return 1;
+    }
 
-    public function toArray();
+    /**
+     * Test to determine if a propery name is an attribute of  this class.
+     * @param string $property
+     * @return boolean
+     */
+    public function isProperty($property)
+    {
 
-    public function get();
+        return property_exists($this, $property);
 
-    public function set(array $list);
+    }
+
+    /**
+     * Returns an array representation of this class.
+     * @return array
+     */
+    public function toArray()
+    {
+
+        $list = array ();
+        $property = NULL;
+        $value = NULL;
+
+        foreach ($this as $property => &$value)
+        {
+            $list[$property] = $value;
+        }
+
+        return $list;
+
+    }
+
+    /**
+     * Overwrites properties of this object.
+     * @param array $list
+     * @return \callow\domain\Domain
+     */
+      public function set(array $list)
+    {
+        foreach ($list as $property=>&$value)
+        {
+
+            if($this->isProperty($property))
+                $this->$property = $value;
+        }
+
+        return $this;
+    }
 
 
+    /**
+     * Returns the properties of this object that are set.
+     * @return array
+     */
+    public function get()
+    {
+
+        $set = array();
+
+        foreach($this as $property=>&$value)
+        {
+            if($value !== NULL)
+                $set[$property] = $value;
+        }
+
+        return $set;
+
+    }
 
 }
 
