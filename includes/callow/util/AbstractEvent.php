3238eeb35d2e673834dd6889d7bf4aa83fd65dd6
developer <lmurray@trinistorm.org>
2012-07-14 17:55:13 -0400

    Made interface. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/callow/util/AbstractEvent.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * timestamp Jul 13, 2012 11:17:03 PM
+ *
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package callow\util
+ *
+ *  Top level parent class of all event type classes. A events are varied across applications but are
+ *  used to pass messages or 'snapshots' of a class when something has occured that may require
+ *  external action(s).
+ *
+ */
+
+namespace callow\util;
+
+class AbstractEvent
+{
+
+    /**
+     * Internal reference to the class that fired the event.
+     * @var EventSource $src
+     * @access protected
+     */
+    protected $src;
+
+    public function __construct(EventSource &$src)
+    {
+
+        $this->src = $src;
+
+    }
+
+    /**
+     * Returns a reference to the EventSource object.
+     * @return EventSource
+     */
+    public function getSource()
+    {
+        return $this->src;
+    }
+
+}
+
+?>
--- includes/callow/util/Event.php
+++ includes/callow/util/Event.php
@@ -1,47 +1,23 @@
 <?php
 
 /**
- * timestamp Jul 13, 2012 11:17:03 PM
- *
+ * timestamp Jul 14, 2012 5:53:45 PM
  *
  *
  * @author Lasana Murray  <dev@trinistorm.org>
  * @copyright 2012 Lasana Murray
  * @package callow\util
  *
- *  Top level parent class of all event type classes. A events are varied across applications but are
- *  used to pass messages or 'snapshots' of a class when something has occured that may require
- *  external action(s).
+ *  Parent interface for all event objects.
  *
  */
 
 namespace callow\util;
 
-class Event
+interface Event
 {
 
-    /**
-     * Internal reference to the class that fired the event.
-     * @var EventSource $src
-     * @access protected
-     */
-    protected $src;
-
-    public function __construct(EventSource &$src)
-    {
-
-        $this->src = $src;
-
-    }
-
-    /**
-     * Returns a reference to the EventSource object.
-     * @return EventSource
-     */
-    public function getSource()
-    {
-        return $this->src;
-    }
+    public function getSource();
 
 }
 
