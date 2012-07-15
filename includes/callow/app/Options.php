6374915aa351bbb88555fbdb29b03901b1074586
developer <lmurray@trinistorm.org>
2012-07-14 22:06:52 -0400

    Fixed typo Signed-off-by:developer <lmurray@trinistorm.org>

--- includes/callow/app/Options.php
+++ includes/callow/app/Options.php
@@ -33,7 +33,7 @@ class Options extends Collection
         if (empty($params[0]))
             array_shift($params);
 
-        parent::__construc($params);
+        parent::__construct($params);
 
     }
 
