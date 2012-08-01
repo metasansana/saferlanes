49ac4120851f68c81ee3998fd9412ebc40980db6
developer <lmurray@trinistorm.org>
2012-08-01 11:13:21 -0400

    Merge the type package into php. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/proof/php/String.php
@@ -0,0 +1,25 @@
+<?php
+namespace proof\php;
+/**
+ * timestamp Jul 23, 2012 5:06:46 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\types
+ *
+ *  Class for simulating a String type.
+ * @todo Incomplete class
+ *
+ */
+class String extends Type
+{
+
+    public function __construct($value)
+    {
+        parent::__construct((string)($value));
+
+    }
+
+
+}
