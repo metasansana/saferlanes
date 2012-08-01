89228e3897bdd6a692f4cc264f855bbc617d9047
developer <lmurray@trinistorm.org>
2012-07-31 20:37:54 -0400

    Browser events Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/proof/webapp/PathRequestEvent.php
@@ -0,0 +1,19 @@
+<?php
+namespace proof\webapp;
+/**
+ * timestamp Jul 30, 2012 4:54:36 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\webapp
+ *
+ *  Event fired whenever the url path has changed.
+ *
+ *
+ */
+
+class PathRequestEvent extends GetRequestEvent
+{
+
+}
---
+++ includes/proof/webapp/PostRequestEvent.php
@@ -0,0 +1,25 @@
+<?php
+namespace proof\webapp;
+
+/**
+ * timestamp Jul 31, 2012 7:57:56 PM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package proof\webapp
+ *
+ *  Event fired when a POST request is detected.
+ *
+ */
+use proof\util\Map;
+
+class PostRequestEvent extends BrowserEvent
+{
+
+    public function __construct(Browser $browser, Map $get)
+    {
+        parent::__constuct($browser, $get);
+    }
+
+}
