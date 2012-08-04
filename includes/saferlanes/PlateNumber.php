668c4afe5500dd9d0d52bea5794b919f7553948d
developer <lmurray@trinistorm.org>
2012-08-04 15:19:03 -0400

    Created the PlateNumber class. Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/saferlanes/PlateNumber.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace saferlanes;
+
+
+/**
+ * timestamp Jul 29, 2012 9:35:48 AM
+ *
+ *
+ * @author Lasana Murray  <dev@trinistorm.org>
+ * @copyright 2012 Lasana Murray
+ * @package saferlanes
+ *
+ *  A plate number class.
+ *
+ */
+class PlateNumber
+{
+
+    const PLATE_REGEX = '/^(p|d|r|h|v|x|t{1})([a-z]){0,2}(\d){1,4}$/';
+
+    /**
+     * The plate number
+     * @var proof\types\String plate
+     * @access private
+     */
+    private $plate;
+
+    public function __construct($plate_number)
+    {
+
+        $this->plate = $plate;
+
+    }
+
+    public function isValid()
+    {
+        return preg_match(Plate::PLATE_REGEX, $this->plate);
+
+    }
+
+    public function getPlate()
+    {
+        return $this->plate;
+    }
+
+    public function __toString()
+    {
+        return $this->plate;
+
+    }
+
+}
