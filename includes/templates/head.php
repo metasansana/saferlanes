87f454f4224bada9f362b0d152837bddc72b5ec7
developer <lmurray@trinistorm.org>
2012-08-05 13:09:12 -0400

    Prepare for final Signed-off-by:developer <lmurray@trinistorm.org>

---
+++ includes/templates/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/screenforce.css" type="text/css" media="screen, projection"/>
+        <link rel="stylesheet" href="css/layout.css" type="text/css" media="screen, projection"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title><?= @$page['title']; ?></title>
+    </head>
+    <body>
+        <div id="master">
+            <div id="window">
--- includes/templates/search.php
+++ includes/templates/search.php
@@ -1,21 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="css/layout.css" type="text/css" media="screen, projection"/>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-        <title><?= $content['title']; ?></title>
-    </head>
-    <body>
-        <div id="master">
-            <div id="window">
                 <img src="img/van.png" alt="truck"/>
                 <h1>Saferlanes</h1>
                 <p>The online road reputation tracker for Trinidad and Tobago.</p>
                 <p>See what the rest of the country thinks about your driving!</p>
                 <div id="application">
-                    <form name="plate_number" method="POST" action="/">
-                        <?= $content['msg']; ?>
-                        <?= $content['plate']; ?>
+                    <?= @$page['msg']; ?>
+                    <form name="plate_number" method="POST" action="/search">
                         <input id="pnum" class="title" type="text" name="plate" placeholder="Driver search"/>
                         <input  type="submit" value="go" name="submit"  />
                     </form>
@@ -24,7 +13,3 @@
                 <span>Got a number to add? Click <a href="/post">here</a>.</span>
                 <br/>
                 <br/>
-            </div>
-        </div>
-</body>
-</html>
